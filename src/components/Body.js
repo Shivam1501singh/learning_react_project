5df61ebd6a9fce87c8f2e07ef5626498a1dc920c
Shivam Singh <shivam.alka03@gmail.com>
2026-01-22 02:47:57 +0530

    adding event handlers

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,15 @@ import restaurantList from "../utils/Mockdata";
 const Body=()=>{
     return(
         <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button className="filter-btn"
+          onClick={()=>{
+            filteredRestaurant=restaurantList.filter((res)=>res.info.avgRating>4.3);
+            console.log(filteredRestaurant);
+          }}
+          >Top rated resturants</button>
+
+        </div>
         <div className="res-container">
             {
               restaurantList.map((resturant)=>(
